511f01bc91d7cd46f818a3d894b2f3f6890ca62b
drkittendorf <60675599+drkittendorf@users.noreply.github.com>
2020-06-23 12:50:14 -0500

    questions written in commented code

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 // array of questions for user
 const questions = [
-
+    // "What is your project title?" **title of ReadMe
+    // "What is the description of your project?" #Description
+    // "Please enter your installation instructions" #Installation Instructions
+    // "Enter any usage information" #Usage
+    // " What are your guidelines for contributing to this project?" #Contributing
+    // "Do you have test instructions?" #Tests
+    // "What level of license? - https://choosealicense.com/licenses/" #license
+    // "What is your github username?" enter under ##Questions
+    // "What is your email address?" enter under # Questions
 ];
 
 // function to write README file
